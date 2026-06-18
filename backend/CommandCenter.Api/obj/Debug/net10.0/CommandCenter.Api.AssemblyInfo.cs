4eb206ab2e976b0c3d185d5462e63b0711fc4428
unknown <chimaobiu@yahoo.com>
2026-06-18 08:06:32 +0100

    Downgrade to .NET 8 LTS for Railway again

--- backend/CommandCenter.Api/obj/Debug/net10.0/CommandCenter.Api.AssemblyInfo.cs
+++ backend/CommandCenter.Api/obj/Debug/net10.0/CommandCenter.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandCenter.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ff31018402d16f25afef9ed8a98c9ddc99665e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ada9b0b0f23da4c8680e41ee70ac93fad70baa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandCenter.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandCenter.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
